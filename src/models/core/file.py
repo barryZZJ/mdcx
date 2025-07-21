fa2b5ce3b56f9888dc88138abb087148a9102b9a
barryZZJ <850462618@qq.com>
2025-07-21 10:18:33 +0800

    更新、读取模式下不跳过已刮削成功的文件

--- src/models/core/file.py
+++ src/models/core/file.py
@@ -905,7 +905,10 @@ async def movie_lists(escape_folder_list: list[str], movie_type: str, movie_path
     total = []
     file_type = movie_type.split("|")
     skip_list = ["skip", ".skip", ".ignore"]
-    not_skip_success = bool("skip_success_file" not in config.no_escape)
+    not_skip_success = bool(
+        "skip_success_file" not in config.no_escape
+        or ("scrape_success_file" in config.no_escape and config.main_mode in [3, 4])
+    )
 
     signal.show_traceback_log("🔎 遍历待刮削目录....")
 
